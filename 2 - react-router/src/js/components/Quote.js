4e16cc5ed0a7c7405658707550daf5f7002e9f22
Danilo Cecilia <danilo.cecilia@gmail.com>
2018-08-09 09:53:53 -0400

    Style quotes

--- 2 - react-router/src/js/components/Quote.js
+++ 2 - react-router/src/js/components/Quote.js
@@ -2,10 +2,15 @@ import React from "react";
 
 export default class Quote extends React.Component {
   render() {
-    const {title, description} = this.props;
+    const { title, description } = this.props;
+
+    const divStyle = {
+      maxWidth: '300px',
+      margin: '10px'
+    }
 
     return (
-      <div class="card border-secondary mb-3">
+      <div class="d-inline-flex card border-secondary mb-3" style={divStyle}>
         <div class="card-header">{title}</div>
         <div class="card-body">
           <p class="card-text">{description}</p>
--- 2 - react-router/src/js/pages/Home.js
+++ 2 - react-router/src/js/pages/Home.js
@@ -15,8 +15,16 @@ export default class Home extends React.Component {
             {
                 title: "Rubens Cecilia",
                 description: "The best and most beautiful things in the world cannot be seen or even touched - they must be felt with the heart."
+            },
+            {
+                title: "Rubens Cecilia",
+                description: "The best and most beautiful things in the world cannot be seen or even touched - they must be felt with the heart."
+            },
+            {
+                title: "Rubens Cecilia",
+                description: "The best and most beautiful things in the world cannot be seen or even touched - they must be felt with the heart."
             }
-        ].map(({title, description}, i) => <Quote key={i} title={title} description={description}/>)
+        ].map(({ title, description }, i) => <Quote key={i} title={title} description={description} />)
         // ].map((q, i) => {
         //     debugger;
         //     return <Quote key={i} title={q.title} description={q.description} />
